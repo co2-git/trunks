f3917bbae828f9787b79d0db380af525495904be
co2-git <francoisrvespa@gmail.com>
2016-07-05 21:08:54 -0500

    chill in

---
+++ app/Trunk.js
@@ -0,0 +1,19 @@
+export default class Trunk {
+  constructor(elem) {
+    this.elem = elem;
+  }
+  get() {
+    return this.constructor.store;
+  }
+  set(value) {
+    const _Trunk = this.constructor;
+    _Trunk.store = {
+      ..._Trunk.store,
+      ...value,
+    };
+    this.elem.setState({[_Trunk.name]: {
+      ..._Trunk.store,
+      ...value,
+    }});
+  }
+}
---
+++ app/connect.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {View} from 'reactors';
+import _ from 'lodash';
+
+class Connector extends Component {
+  state = this.props.stores;
+  parseChildProps() {
+    const props = {};
+    for (const store_name of _.keys(this.props.stores)) {
+      const store = new (this.props.stores[store_name])(this);
+      props[store_name] = store;
+    }
+    return props;
+  }
+  render() {
+    return (
+      <View>
+        {React.cloneElement(this.props.children, this.parseChildProps())}
+      </View>
+    );
+  }
+}
+
+function curryActions(actions, connector) {
+  const curry = {};
+  for (const action_name in actions) {
+    const action = actions[action_name];
+    curry[action_name] = (...props) => {
+      const wrapper = action(...props);
+      return wrapper({
+        update: (value) => connector.setState({value}),
+      });
+    };
+  }
+  return curry;
+}
+
+export default function connect(Component, stores) {
+  return props => (
+    <Connector stores={stores}>
+      <Component {...props} />
+    </Connector>
+  );
+}
